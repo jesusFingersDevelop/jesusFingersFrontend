ff59a7dd39faebc15e429c706fda157fc15ea01e
11t518s <11t518s20161759@gmail.com>
2021-09-22 21:59:21 +0900

    feature: initialSettings ues by babel, webpack #1

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const webpack = require("webpack");
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: { react_app: "./src/index.js" },
+  resolve: { extensions: [".js", ".jsx", ".css"] },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: "/node_modules/",
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+  ],
+  devServer: {
+    host: "localhost",
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+};
