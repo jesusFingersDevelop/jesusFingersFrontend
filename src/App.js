0b1a0cec5193a7aee5d7d3fe4107f865a6d76459
11t518s <11t518s20161759@gmail.com>
2021-09-24 13:53:53 +0900

    chore: eslint 재설정

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React from 'react';
 import './App.scss';
 
 function App() {
+  const a = 'asdf';
+  const b = () => {
+    console.log('asdf');
+  };
+
   return (
     <div>
       <h1>jesusFingers 시작~~~~~~~~~~~~~~~~~ 화이팅화이팅~~~~~~~~~~~~~~</h1>
+      asdfsfsf
     </div>
   );
 }
