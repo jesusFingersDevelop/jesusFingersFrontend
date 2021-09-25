7b14133532ca9ebd1ec242e7063c9778274ff14e
11t518s <11t518s20161759@gmail.com>
2021-09-25 23:55:13 +0900

    fix: eslint but fail #9

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import './App.scss';
 
 function App() {
+  const a = 'sdf';
+  const b = 'sadf';
+  const c = 'ssdfadf';
   return (
     <div>
-      <h1>jesusFingers 시작~~~~~~~~~~~~~~~~~ 화이팅화이팅~~~~~~~~~~~~~~</h1>
+      <h1>jesusFingers 시작~~~~~~~~~~ 화이팅화이팅~~~~~~~~~~</h1>
     </div>
   );
 }
