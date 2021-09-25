fb910494256ec33732d5aae585d6d5db0ccc5a34
11t518s <69836010+11t518s@users.noreply.github.com>
2021-09-26 01:52:06 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import Test from 'Components/test/Test';
 import React from 'react';
 import './App.scss';
 
@@ -6,7 +5,6 @@ function App() {
   const a = 'test';
   return (
     <div>
-      <Test />
       <h1> jesusFingers 시작~~~~~~~~~~ 화이팅화이팅~~~~~~~~~~</h1>
     </div>
   );
